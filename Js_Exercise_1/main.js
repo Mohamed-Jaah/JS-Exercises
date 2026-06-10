a4f303554286c0671764b2ce7782d4f79005ce98
Mohamed-Jaah <maxammedcabdalle@gmail.com>
2026-06-10 23:31:35 +0300

    Submitting my Js-Exercises folder and my first Js-Exercise files to my Github Account

---
+++ Js_Exercise_1/main.js
@@ -0,0 +1,22 @@
+let x = 7;
+let y = 3;
+
+console.log("Isu gaynta labadan tiro waa " + ":" + " " + (x + y));
+console.log("Kala goynta labadan tiro wa " + ":" + " " + (x - y));
+console.log("Isku dhufashada labadan tiro waa " + ":" + " " + (x * y));
+console.log("Qaybinta labadan tiro waa " + ":" + " " + (x / y));
+console.log("Hadhaaga labadan tiro marka la isku qaybiyo waa " + ":" + " " + (x % y));
+
+// Pre Increment
+console.log(x++);
+console.log("Pre Increment of this number is : "+x);
+
+// Post Increment
+console.log(x++);
+console.log("Post Increment of this number is : "+x);
+
+
+
+// Post Decrement
+console.log(x--);
+console.log(x);
