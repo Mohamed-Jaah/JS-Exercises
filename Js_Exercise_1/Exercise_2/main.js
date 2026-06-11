a41b47e9244052d942021d22dada0c245daab390
Mohamed-Jaah <maxammedcabdalle@gmail.com>
2026-06-11 06:59:08 +0300

    Submitting Second exercise_2 and some folder updates

---
+++ Js_Exercise_1/Exercise_2/main.js
@@ -0,0 +1,50 @@
+x = 15;
+y = "15";
+
+// Equality Operator
+if (x == y)
+ console.log("X has equal value to Y");
+else
+ console.log("X does not have equal value to Y");
+
+// Not Equality Operator
+if(x != y)
+ console.log("X does not have equal value to Y");   
+else
+ console.log("X has equal value to Y");
+
+// Strict Equality Operator
+if(x === y)
+ console.log("X has equal type and value to Y");
+else
+ console.log("X does not have equal type and value to Y");
+
+// Strict Not Equality Operator
+if(x !== y)
+ console.log("X does not have equal type and value to Y");  
+else
+ console.log("X has equal type and value to Y");
+
+// Greater than operator
+if (x > y)
+    console.log("X is greater than y");
+else
+    console.log("X is not equal to y");
+
+// Less than operator
+if (x < y )
+    console.log("X is less than y");
+else
+    console.log("X is not less than y");
+
+// Greater than or equal to operator
+if(x >= y)
+    console.log("X is greater than or equal to Y");
+else
+CONSOLE.LOG("X is not grater than or equal to y");
+
+// Less than or equal to operator
+if(x <= y)  
+    console.log("X is less than or equal to Y");
+else
+    console.log("X is not less than or equal to Y");
